ef3339f06415ddac1c7cc131f3b9dbede4790c33
J Carlos Rojas <j.carlosrojas@outlook.com>
2024-01-22 07:50:15 -0500

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors:{
+        'ut_color' : '#5DC1B9'
+      }
+    },
+  },
+  plugins: [],
+}
+
